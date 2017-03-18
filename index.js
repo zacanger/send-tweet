96ce096dbcbabd883b33beaf8938a306b04c9553
zacanger <zac@zacanger.com>
2017-03-18 12:06:44 -0600

    Fix help text.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ if (module.parent) {
 
 const help = () => `
   send-tweet
+  ----------
   usage: send-tweet 'A tweet!'
 `
 
@@ -22,7 +23,7 @@ const access_token_secret = process.env.SEND_TWEET_ACCESS_TOKEN_SECRET
 if (!consumer_key || !consumer_secret || !access_token_key || !access_token_secret) {
   console.log(`
   Please go to apps.twitter.com and click 'Create New App'
-  Then set the following environment variables (in your '~/.bashrc or equivalent'):
+  Then set the following environment variables (in your '~/.bashrc' or equivalent):
   SEND_TWEET_CONSUMER_KEY
   SEND_TWEET_CONSUMER_SECRET
   SEND_TWEET_ACCESS_TOKEN_KEY
