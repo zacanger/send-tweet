fabf66d322f30e7c0f6a042fb645878903129326
zacanger <zac@zacanger.com>
2020-03-22 12:07:34 -0600

    chore: switch to eslint

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const consumer_secret = process.env.SEND_TWEET_CONSUMER_SECRET
 const access_token_key = process.env.SEND_TWEET_ACCESS_TOKEN_KEY
 const access_token_secret = process.env.SEND_TWEET_ACCESS_TOKEN_SECRET
 
-if (!consumer_key || !consumer_secret || !access_token_key || !access_token_secret) {
+if (
+  !consumer_key ||
+  !consumer_secret ||
+  !access_token_key ||
+  !access_token_secret
+) {
   console.log(`
   Please go to apps.twitter.com and click 'Create New App'
   Then set the following environment variables (in your '~/.bashrc' or equivalent):
@@ -37,16 +42,20 @@ const auth = {
   consumer_key,
   consumer_secret,
   access_token_key,
-  access_token_secret
+  access_token_secret,
 }
 
 const client = new Twitter(auth)
 const theTweet = argv[2]
-if (!theTweet || argv[3]) help()
+if (!theTweet || argv[3]) {
+  help()
+}
 
 const main = (a) =>
   client.post('statuses/update', { status: a }, (err, tweet) => {
-    if (err) return console.warn('Error:', err[0].message)
+    if (err) {
+      return console.warn('Error:', err[0].message)
+    }
     console.log(`Tweet '${tweet.text}' sent!`)
   })
 
